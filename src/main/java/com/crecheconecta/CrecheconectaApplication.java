bcda453b3aba988750348255bf290264aee74940
Murilo Novaes de Oliveira <130521405+MuriloN0@users.noreply.github.com>
2026-09-07 18:03:09 -0300

    feat: Base spring initializer

---
+++ src/main/java/com/crecheconecta/CrecheconectaApplication.java
@@ -0,0 +1,13 @@
+package com.crecheconecta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrecheconectaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrecheconectaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crecheconecta/CrecheconectaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crecheconecta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrecheconectaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
